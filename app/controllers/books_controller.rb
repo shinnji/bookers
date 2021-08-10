ccf101a6ecf6f02a306570137a1a8390c9673e1d
shinnji <ec2-user@ip-172-31-21-242.ec2.internal>
2021-08-10 13:08:15 +0000

    book-destroy-end

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,11 +20,17 @@ class BooksController < ApplicationController
     book.save
     redirect_to book_path(book)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
 
   private
