53faa18a3349948a16eae9a7f2891457aaf86a24
shinnji <ec2-user@ip-172-31-21-242.ec2.internal>
2021-08-10 06:24:21 +0000

    book-index-end

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -11,16 +12,22 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
     redirect_to '/books/:id'
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
